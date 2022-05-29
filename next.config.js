488a2d4bb696f26886b4f81b32d898564c319b3c
Ved Rane <74463375+Get-DevVed-YT@users.noreply.github.com>
2022-05-29 00:59:41 +0000

    Ignore ESLint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
   pwa: {
     dest: 'public',
     runtimeCaching
